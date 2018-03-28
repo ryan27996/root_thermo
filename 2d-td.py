2d8606d349f46499916587ae7fe08cc6e5d8d8b6
Ryan Walker <rwalke50@emich.edu>
2018-03-28 15:46:01 -0400

    Working with dtypes

--- 2d-td.py
+++ 2d-td.py
@@ -43,9 +43,16 @@ nx, ny = int(w/dx), int(h/dy)
 
 dx2, dy2 = dx*dx, dy*dy
 dt = dx2 * dy2 / (2 * D * (dx2 + dy2))
+gridArray = np.dtype([('Temp', np.float64),
+                      ('StateChange', np.bool),
+                      ('Energy', np.float64)])
 
-u0 = Tcool * np.ones((nx, ny))
-u = np.empty((nx, ny))
+u0 = np.empty((nx, ny), gridArray)
+u = np.empty((nx, ny), gridArray)
+u0[:, :]['Temp'] = Tcool
+u0[:, :]['StateChange'] = False
+u0[:, :]['Energy'] = 0.0
+
 
 # Initial conditions - ring of inner radius r, width dr centred at (cx,cy) (mm)
 r, cx, cy = 16, w/2., h/2.
@@ -61,26 +68,26 @@ for i in range(nx):
     for j in range(ny):
         p2 = (i*dx-cx)**2 + (j*dy-cy)**2
         if p2 < r2:
-            u0[i+hrr, j] = Thot
-            u0[i-hrr, j] = Thot
-            u0[i, j+hrr] = Thot
-            u0[i, j-hrr] = Thot
+            u0[i+hrr, j]['Temp'] = Thot
+            u0[i-hrr, j]['Temp'] = Thot
+            u0[i, j+hrr]['Temp'] = Thot
+            u0[i, j-hrr]['Temp'] = Thot
 
 
 def getMeltRadius(array, melt_temp):
     length = len(array)
     xc = int(length / 2)  # Centerpoint
     for y in range(0, length - 1):
-        if array[xc, y] > melt_temp:
+        if array[xc, y]['Temp'] > melt_temp:
             return xc - y
 
 
 def do_timestep(u0, u):
 
     # Propagate with forward-difference in time, central-difference in space
-    u[1:-1, 1:-1] = u0[1:-1, 1:-1] + D * dt * (
-          (u0[2:, 1:-1] - 2*u0[1:-1, 1:-1] + u0[:-2, 1:-1])/dx2
-          + (u0[1:-1, 2:] - 2*u0[1:-1, 1:-1] + u0[1:-1, :-2])/dy2
+    u[1:-1, 1:-1]['Temp'] = u0[1:-1, 1:-1]['Temp'] + D * dt * (
+          (u0[2:, 1:-1]['Temp'] - 2*u0[1:-1, 1:-1]['Temp'] + u0[:-2, 1:-1]['Temp'])/dx2
+          + (u0[1:-1, 2:]['Temp'] - 2*u0[1:-1, 1:-1]['Temp'] + u0[1:-1, :-2]['Temp'])/dy2
           )
 
     mr = int(getMeltRadius(u0, Tmelt)*dx)
@@ -90,10 +97,10 @@ def do_timestep(u0, u):
         for j in range(ny):
             p2 = (i*dx-cx)**2 + (j*dy-cy)**2
             if p2 < r2:
-                u[i+dr, j] = Thot
-                u[i-dr, j] = Thot
-                u[i, j+dr] = Thot
-                u[i, j-dr] = Thot
+                u[i+dr, j]['Temp'] = Thot
+                u[i-dr, j]['Temp'] = Thot
+                u[i, j+dr]['Temp'] = Thot
+                u[i, j-dr]['Temp'] = Thot
     u0 = u.copy()
     return u0, u
 
@@ -121,7 +128,7 @@ while t <= nsteps:
             color='r', linestyle='dashed', fill=False, linewidth=2.0)
         ax = plt.gca()
         ax.add_artist(circle2)
-        cp1 = plt.contourf(X, Y, u.copy(), 20)
+        cp1 = plt.contourf(X, Y, u['Temp'].copy(), 20)
 
         plt.title(
             'Contour Plot of Tempeture in ice' +
@@ -132,21 +139,3 @@ while t <= nsteps:
         plt.colorbar(cp1)
         plt.savefig("out-{0}.png".format(str(int(t*dt)).zfill(6)))
         plt.close()
-
-'''
-fig = plt.figure()
-for m in range(nsteps):
-    u0, u = do_timestep(u0, u)
-    if m in mfig:
-        fignum += 1
-        print(m, fignum)
-        ax = fig.add_subplot(220 + fignum)
-        im = ax.imshow(u.copy(), cmap=plt.get_cmap('hot'), vmin=Tcool, vmax=Thot)
-        ax.set_axis_off()
-        ax.set_title('{:.1f} ms'.format(m*dt*1000))
-fig.subplots_adjust(right=0.85)
-cbar_ax = fig.add_axes([0.9, 0.15, 0.03, 0.7])
-cbar_ax.set_xlabel('$T$ / K', labelpad=20)
-fig.colorbar(im, cax=cbar_ax)
-plt.show()
-'''
